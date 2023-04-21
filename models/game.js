5d1a742f75d8e4dc580fb2738e7f5a2f9543c7ea
Eunice Yang <euniceyang21@gmail.com>
2023-04-21 16:46:25 -0500

    Add models

---
+++ models/game.js
@@ -0,0 +1,85 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+  content: String,
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5, 
+    default: 3,
+  },
+  owner: { type: Schema.Types.ObjectId, ref: 'Profile' },
+  game: { type: Schema.Types.ObjectId, ref: 'Game' },
+})
+
+const gameSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  imgUrl: String,
+  category: {
+    type: String,
+    enum: ['Co-Op', 'Engine Builder', 'Deck Builder', 'Worker Placement', 'Social Deduction', 'RPG'],
+    default: 'Co-Op',
+  },
+  duration: {
+    type: String,
+    enum: ['> 30 min', '1 - 1.5 hrs', '2+ hrs'],
+    default: '1 - 1.5 hrs',
+  },
+  minPlayers: {
+    type: Number,
+    min: 1,
+    default: 1,
+  },
+  maxPlayers: {
+    type: Number,
+    min: 1,
+    default: 4,
+  },
+  complexity: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 3,
+  },
+  strategy: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 3,
+  },
+  luck: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 3,
+  },
+  online: Boolean,
+  year: {
+    type: Number,
+    default: function(){
+      return new Date().getFullYear()
+    },
+  },
+  votes: {
+    type: Number,
+    default: 0,
+  },
+
+  reviews: [reviewSchema],
+  owner: { type: Schema.Types.ObjectId, ref: 'Profile' },
+}, {
+  timestamps: true
+})
+
+const Game = mongoose.model('Game', gameSchema)
+
+export {
+  Game,
+  reviewSchema
+}
--- models/profile.js
+++ models/profile.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
+import { reviewSchema } from './game.js'
 
 const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  favorites: { type: Schema.Types.ObjectId, ref: 'Game' },
+  reviews: [reviewSchema],
 }, {
   timestamps: true
 })
