058e3add97dc4cd07b6990056cfabd15033f93cf
Eunice Yang <euniceyang21@gmail.com>
2023-04-23 11:32:31 -0500

    Add route for show controller

--- routes/games.js
+++ routes/games.js
@@ -10,6 +10,9 @@ router.get('/', gamesCtrl.index)
 // GET localhost:3000/games/new
 router.get('/new', isLoggedIn, gamesCtrl.new)
 
+// GET localhost:3000/games/:gameId
+router.get('/:gameId', gamesCtrl.show)
+
 // POST localhost:3000/games
 router.post('/',  isLoggedIn, gamesCtrl.create)
 
