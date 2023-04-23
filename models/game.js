5c16012abb8dd4d071ee98faef34e8d6dadba2ab
Eunice Yang <euniceyang21@gmail.com>
2023-04-22 19:26:45 -0500

    Draft styling of games/index view

--- models/game.js
+++ models/game.js
@@ -28,7 +28,7 @@ const gameSchema = new Schema({
   },
   duration: {
     type: String,
-    enum: ['> 30 min', '1 - 1.5 hrs', '2+ hrs'],
+    enum: ['< 30 min', '1 - 1.5 hrs', '2+ hrs'],
     default: '1 - 1.5 hrs',
   },
   minPlayers: {
