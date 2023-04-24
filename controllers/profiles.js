be87194e2ad4cdb9f1e0123a1ef75d0fea64aecf
Eunice Yang <euniceyang21@gmail.com>
2023-04-24 13:49:02 -0500

    Update profile view

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,26 +1,29 @@
 import { Profile } from '../models/profile.js'
 import { Game } from '../models/game.js'
 
-function index(req, res) {
-  Profile.find({})
-  .then(profiles => {
-    res.render('profiles/index', {
-      profiles,
-			title: "🐐"
-    })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/')
-  })
-}
+// function index(req, res) {
+//   Profile.find({})
+//   .then(profiles => {
+//     res.render('profiles/index', {
+//       profiles,
+// 			title: "🐐"
+//     })
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     res.redirect('/')
+//   })
+// }
 
 function show (req, res){
   Profile.findById(req.user.profile._id)
+  .populate('favorites')
   .then(profile => {
+    const favorites = profile.favorites
     res.render('profiles/show', {
       title: `${profile.name}'s profile`,
       profile,
+      favorites,
     })
   })
   .catch(err => {
@@ -33,7 +36,7 @@ function addToFavList (req, res){
   Game.findById(req.params.gameId)
   .then(game => {
     Profile.findById(req.user.profile._id)
-    .populate("favorites")
+    .populate('favorites')
     .then(profile => {
       profile.favorites.push(game._id)
       profile.save()
@@ -57,7 +60,7 @@ function addToFavList (req, res){
 }
 
 export {
-  index,
+  // index,
   show,
   addToFavList,
 }
